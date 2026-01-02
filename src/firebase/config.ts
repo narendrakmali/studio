6a14853de180174293b33f484551926f0978af22
Narendra Mali <narendrakmali@gmail.com>
2026-01-02 10:48:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8218423802-52bea",
+  "appId": "1:292895703672:web:7b474d0d1adb41e19f238d",
+  "apiKey": "AIzaSyD76sbhZeUXegRJWwvkt7nilrgaCoexgzA",
+  "authDomain": "studio-8218423802-52bea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "292895703672"
+};
