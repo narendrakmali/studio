5994783d3e229f40098905305ff8ae510347a750
Narendra Mali <narendrakmali@gmail.com>
2026-01-02 17:40:52 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
+
 import type {NextConfig} from 'next';
 
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV !== 'development'
+  disable: process.env.NODE_ENV === 'development'
 });
 
 const nextConfig: NextConfig = {
@@ -40,4 +41,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
