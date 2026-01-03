3ec0d9214144b1e88da6c3acffa875e47db58fbc
narendrakmali <narendrakmali@gmail.com>
2026-01-03 17:13:50 +0530

    devnix updated

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,11 +9,7 @@
     pkgs.zulu
   ];
   # Sets environment variables in the workspace
-  env = {
-    NEXT_PUBLIC_FIREBASE_API_KEY = "AIzaSyCdJBk30077iZbcbHQf2vxg7ZTMx8st3k0";
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "studio-8218423802-52bea.firebaseapp.com";
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "studio-8218423802-52bea";
-  };
+  env = {};
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
@@ -36,7 +32,7 @@
     };
     # Enable previews and customize configuration
     previews = {
-      enable = false;
+      enable = true;
       previews = {
         web = {
           command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
